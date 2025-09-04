9725cb60c6052c96452417aec6e2c027ff00c0d8
Kenzo <satteliw@gmail.com>
2025-09-04 14:28:52 -0300

    aula90 completa

---
+++ JAVASCRIPT/aulas_js/aula90_API(GET)/assets/main.js
@@ -0,0 +1,53 @@
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest(); // xhr é XML Http Request
+//         xhr.open(obj.method , obj.url, true);
+//         xhr.send();
+    
+//         xhr.addEventListener('load', () => {
+//         if(xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     });
+// };
+
+// fetch('pagina1.html')
+// .then(response => {
+//     if(response.status !== 200) throw new Error('ERRO 404 NOSSO')
+//         return response.text();
+// })
+// .then(html => console.log(html))
+// .catch(e => console.error('ERRO: ', e));
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+    if(tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    try{
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+    
+        if(response.status !== 200) throw new Error('ERRO 404!');
+        const html = await response.text();
+        carregaResultado(html);
+    } catch(e){
+        console.error(e);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
+
+
+
